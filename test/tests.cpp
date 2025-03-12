b5f24c74b688d277856374d8a0385f69bf1620de
anton <ashtanyuk@gmail.com>
2025-03-12 14:30:35 +0300

    Update files

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,46 +5,46 @@
 #include "alg.h"
 
 TEST(seqCollatz, test1) {
-   unsigned int maxlen, number;
-   number = seqCollatz(&maxlen, 1, 1000000);
-   ASSERT_EQ(525, maxlen);
-   ASSERT_EQ(837799, number);
+  unsigned int maxlen, number;
+  number = seqCollatz(&maxlen, 1, 1000000);
+  ASSERT_EQ(525, maxlen);
+  ASSERT_EQ(837799, number);
 }
 
 TEST(seqCollatz, test2) {
-   unsigned int maxlen, number;
-   number = seqCollatz(&maxlen, 1, 30);
-   ASSERT_EQ(112, maxlen);
-   ASSERT_EQ(27, number);
+  unsigned int maxlen, number;
+  number = seqCollatz(&maxlen, 1, 30);
+  ASSERT_EQ(112, maxlen);
+  ASSERT_EQ(27, number);
 }
 
 TEST(seqCollatz, test3) {
-   unsigned int maxlen, number;
-   number = seqCollatz(&maxlen, 1, 100);
-   ASSERT_EQ(119, maxlen);
-   ASSERT_EQ(97, number);
+  unsigned int maxlen, number;
+  number = seqCollatz(&maxlen, 1, 100);
+  ASSERT_EQ(119, maxlen);
+  ASSERT_EQ(97, number);
 }
 
 TEST(collatzLen, test1) {
-   unsigned int len;
-   len = collatzLen(27);
-   ASSERT_EQ(112, maxlen);
+  unsigned int len;
+  len = collatzLen(27);
+  ASSERT_EQ(112, len);
 }
 
 TEST(collatzLen, test2) {
-   unsigned int len;
-   len = collatzLen(3);
-   ASSERT_EQ(8, maxlen);
+  unsigned int len;
+  len = collatzLen(3);
+  ASSERT_EQ(8, len);
 }
 
 TEST(collatzMaxValue, test1) {
-   unsigned int number;
-   number = collatzMaxValue(27);
-   ASSERT_EQ(9232, number);
+  unsigned int number;
+  number = collatzMaxValue(27);
+  ASSERT_EQ(9232, number);
 }
 
 TEST(collatzMaxValue, test2) {
-   unsigned int number;
-   number = collatzMaxValue(3);
-   ASSERT_EQ(16, number);
+  unsigned int number;
+  number = collatzMaxValue(3);
+  ASSERT_EQ(16, number);
 }
