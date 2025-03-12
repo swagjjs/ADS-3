740955aaa56f1920046c75a28870c6b6a319de4e
anton <ashtanyuk@gmail.com>
2025-03-12 14:34:12 +0300

    Update files

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,8 +10,8 @@ unsigned int collatzLen(uint64_t num) {
   return 1;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, 
-                        uint64_t lbound, 
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
                         uint64_t rbound) {
   return 1;
 }
