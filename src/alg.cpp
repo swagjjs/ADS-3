ecda32513b34c71d6eb9b39d9820612284c5ea11
swagjjs <liazemcova00@gmail.com>
2025-06-08 11:21:36 +0300

    alg3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,58 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t max_value = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+        num /= 2;
+    } else {
+        num = num * 3 + 1;
+    }
+        
+    if (num > max_value) {
+        max_value = num;
+    }
+  }
+    
+  return max_value;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int length = 1;  
+    
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    length++;
+  }
+  return length;
+  
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  if (maxlen == nullptr || lbound < 2 || lbound > rbound) {
+    return 0;
+  }
+    
+  unsigned int max_length = 0;
+  unsigned int result_num = 0;
+    
+  for (uint64_t num = lbound; num <= rbound; ++num) {
+    unsigned int current_len = collatzLen(num);
+        
+    if (current_len > max_length) {
+      max_length = current_len;
+      result_num = num;
+    }
+  }
+    
+  *maxlen = max_length;
+  return result_num;
+
+  
 }
